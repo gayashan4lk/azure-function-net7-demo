8cff705fb5d0450be2fb9a11cb0018197b07b145
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-03-20 01:56:31 +0530

    delete customer

---
+++ azure-function-net7-demo/Api/CustomerApi/DeleteCustomer.cs
@@ -0,0 +1,38 @@
+using System.Net;
+using System.Text.Json;
+using azure_function_net7_demo.Models;
+using Microsoft.Azure.CosmosRepository;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace azure_function_net7_demo.Api.CustomerApi
+{
+    public class DeleteCustomer
+    {
+        private readonly ILogger _logger;
+        private readonly IRepository<Customer> customerRepository;
+
+        public DeleteCustomer(ILoggerFactory loggerFactory, IRepository<Customer> customerRepository)
+        {
+            _logger = loggerFactory.CreateLogger<CreateCustomer>();
+            this.customerRepository = customerRepository;
+        }
+
+        [Function("DeleteCustomer")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "customer/{id}")] HttpRequestData req, string id)
+        {
+            _logger.LogInformation("Deleting a customer.");
+
+            if(string.IsNullOrEmpty(id)) return req.CreateResponse(HttpStatusCode.BadRequest);
+
+            var customer = await customerRepository.GetAsync(id);
+
+            if(customer == null) return req.CreateResponse(HttpStatusCode.BadRequest);
+
+            await customerRepository.DeleteAsync(customer);
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
